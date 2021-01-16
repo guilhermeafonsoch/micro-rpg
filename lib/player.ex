f92b9ec160827cc0bf06635413879566d412d5b2
Guilherme Afonso <ch.afonso.gui@gmail.com>
2021-01-16 18:09:46 -0300

    Struct do Jogador

---
+++ lib/player.ex
@@ -0,0 +1,19 @@
+defmodule MicroRpg.Player do
+    
+    @enforce_keys [:life, :name, :move_rnd, :move_avg, :move_heal]
+    defstruct [:life, :name, :move_rnd, :move_avg, :move_heal]
+
+    def build(name, move_rnd, move_avg, move_heal) do
+        %MicroRpg.Player{
+            life: 100,
+            name: name,
+            move_rnd: move_rnd,
+            move_avg: move_avg,
+            move_heal: move_heal
+        }
+    end
+
+
+
+
+end
