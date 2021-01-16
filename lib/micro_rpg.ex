53c7c700492c0b267ab5efc9be57c33e4b4247c9
Guilherme Afonso <ch.afonso.gui@gmail.com>
2021-01-16 19:50:30 -0300

    Game Agent

--- lib/micro_rpg.ex
+++ lib/micro_rpg.ex
@@ -1,8 +1,15 @@
 defmodule MicroRpg do
+  alias MicroRpg.{Game, Player}
 
-  alias MicroRpg.Player
+  @nome_adversario "TANJIRO"
+
   def create_player(name, move_rnd,move_avg, move_heal) do
     Player.build(name, move_rnd,move_avg, move_heal)
   end
+
+  def start_game(player) do
+    @nome_adversario |> create_player(:soco, :chute, :curar) |> Game.start(player)
+  end
+
 end
  
